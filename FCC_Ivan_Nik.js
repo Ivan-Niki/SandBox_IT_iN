cdf4671bdf8d59ef174bc757cbb6f6ae02d3ab51
Ivan-Niki <iFankov@gmail.com>
2022-12-15 03:10:34 +0300

    commit fcc_js 15 Dec

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -307,3 +307,14 @@ function fun2() {
   console.log(output);
 }
 
+// Task_50 ==> Local Scope and Functions
+function myLocalScope() {
+  // Only change code below this line
+const myVar = "Hello";
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
