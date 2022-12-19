2a1fd6adbbea3365b003e270919dd70309316ae9
Ivan-Niki <iFankov@gmail.com>
2022-12-19 23:02:31 +0300

    changes fcc_js 19 Dec

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -440,3 +440,63 @@ function testStrictNotEqual(val) {
 }
 
 testStrictNotEqual(10);
+
+// Task_62 ==> Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+// Task_63 ==> Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
+// Task_64 ==> Comparison with the Less Than Operator
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+testLessThan(10);
+
+// Task_65 ==> Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
