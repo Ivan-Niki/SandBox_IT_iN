d93b75032fd4d1997fe2330f662edbc441595389
Ivan-Niki <iFankov@gmail.com>
2022-12-24 19:08:55 +0300

    changes fcc_js fcc_css 24 Dec

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -728,4 +728,11 @@ function chainToSwitch(val) {
 
 chainToSwitch(7);
 
-// Task_77 ==> 
+// Task_77 ==> Returning Boolean Values from Functions
+function isLess(a, b) {
+  // Only change code below this line
+    return (a < b);
+  // Only change code above this line
+}
+
+isLess(10, 15);
