c68c144fb24a4cd0bd434b98db0e9691c8773ef9
Ivan-Niki <iFankov@gmail.com>
2022-12-25 13:45:14 +0300

    changes cw fcc_js fcc_css 25 Dec

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -736,3 +736,190 @@ function isLess(a, b) {
 }
 
 isLess(10, 15);
+
+// Task_78 ==> Return Early Pattern for Functions
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+if (a < 0 || b < 0) {
+  return undefined;
+}
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+// Task_79 ==> Counting Cards
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  count++;
+  break;
+  case 10:
+  case 'J':
+  case 'Q':
+  case 'K':
+  case 'A':
+  count--;
+  break;
+}
+if (count > 0) {
+  return count + " Bet";
+} else {
+  return count + " Hold";
+}
+
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// Task_80 ==> Build JavaScript Objects
+const myDog = {
+  // Only change code below this line
+name: "Jack",
+legs: 5,
+tails: 1,
+friends: ["Kate", "Dima", "Ivan"]
+
+  // Only change code above this line
+};
+
+// Task_81 ==> Accessing Object Properties with Dot Notation
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+
+// Task_82 ==> Accessing Object Properties with Bracket Notation
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+// Task_83 ==> Accessing Object Properties with Variables
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+
+// Task_84 ==> Updating Object Properties
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+
+// Task_85 ==> Add New Properties to a JavaScript Object
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog["bark"] = "woof";
+
+// Task_86 ==> Delete Properties from a JavaScript Object
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
+
+// Task_87 ==> Using Objects for Lookups
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+
+// Task_88 ==> Testing Objects for Properties
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)) {
+  return obj[checkProp];
+    } else {
+      return "Not Found";
+  }
+  // Only change code above this line
+}
+
+// Task_89 ==> Manipulating Complex Objects
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Valeriy Kipelov",
+    "title": "Babylon",
+    "release_year": 2004,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
