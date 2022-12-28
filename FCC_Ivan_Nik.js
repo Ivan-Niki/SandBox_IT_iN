86972029522d6fb8787686c10a823700f2b95e40
Ivan-Niki <iFankov@gmail.com>
2022-12-28 16:27:26 +0300

    2nd chng fcc_js 28 Dec

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1012,3 +1012,27 @@ for (let i = 9; i > 0; i-=2) {
   myArray.push(i);
 }
 
+// Task_96 ==> Iterate Through an Array with a For Loop
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+}
+
+// Task_97 ==> Nesting For Loops
+function multiplyAll(arr) {
+  let product = 1;
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    product *= arr[i][j];
+  }
+}
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
