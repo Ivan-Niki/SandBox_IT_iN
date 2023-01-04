c95d51e805d5b46da6d1ef5462d17e6e4e8af487
Ivan-Niki <iFankov@gmail.com>
2023-01-04 16:56:21 +0300

    chngs cw fcc_js 04 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1079,4 +1079,14 @@ function checkEqual(a, b) {
   
   checkEqual(1, 2);
 
+ // Task_103 ==> Use Multiple Conditional (Ternary) Operators
+ function checkSign(num) {
+  return (num > 0) ? "positive"
+  : (num < 0) ? "negative"
+  : "zero";
+  }
   
+  checkSign(10);
+
+ // Task_104 ==> 
+
