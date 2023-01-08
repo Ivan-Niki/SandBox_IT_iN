a3b7cd59298a3433f89a0276baef018f0ded3ed4
Ivan-Niki <iFankov@gmail.com>
2023-01-08 13:46:40 +0300

    chngs cw fcc_js fcc_css 08 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1142,3 +1142,15 @@ let myArray = ["a", "b", "c", "d"];
 myArray[1] = "changed 1st position";
 // Only change code above this line
 console.log(myArray);
+
+// Task_3 ==> Add Items to an Array with push() and unshift()
+function mixedNumbers(arr) {
+  // Only change code below this line
+arr.push(7, "VIII", 9);
+arr.unshift("I", 2, "three");
+  // Only change code above this line
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
+
