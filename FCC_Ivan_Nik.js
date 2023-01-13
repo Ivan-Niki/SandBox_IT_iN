17da4eb0482b05c1ce9bdf0b0d3ae49bef50b295
Ivan-Niki <iFankov@gmail.com>
2023-01-13 12:57:54 +0300

    chngs cw fcc_js fcc_css 13 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1216,3 +1216,21 @@ function spreadOut() {
 }
 
 console.log(spreadOut());
+
+
+
+
+// Task_9 ==> Check For The Presence of an Element With indexOf()
+
+function quickCheck(arr, elem) {
+  // Only change code below this line
+if (arr.indexOf(elem) >=0) {
+  return true;
+}
+else {
+  return false;
+}
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
