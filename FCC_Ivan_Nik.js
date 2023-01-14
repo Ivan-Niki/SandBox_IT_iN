282e7385344cbdb6f2575a599390ce4bcfc365b5
Ivan-Niki <iFankov@gmail.com>
2023-01-14 18:40:53 +0300

    chngs fcc_js fcc_css 14 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1234,3 +1234,19 @@ else {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+
+// Task_10 ==> Iterate Through All an Array's Items Using For Loops
+function filteredArray(arr, elem) {
+  let newArr = [];
+  // Only change code below this line
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      newArr.push(arr[i]);
+    }
+}
+  // Only change code above this line
+  return newArr;
+}
+
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
