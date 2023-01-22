4d79f5ff5f95d088a387b8ea2c068747f41709ab
Ivan-Niki <iFankov@gmail.com>
2023-01-22 19:16:21 +0300

    chngs fcc_js 22 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1307,7 +1307,23 @@ console.log(userActivity);
 
 
 
-// Task_15 ==> 
+// Task_15 ==> Use the delete Keyword to Remove Object Properties
+let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// Only change code above this line
+
+console.log(foods);
 
 
 // Task_16 ==> 
