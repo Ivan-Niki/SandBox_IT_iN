64a89717dfb86759ff27ca41be4591fb340e5a5a
Ivan-Niki <iFankov@gmail.com>
2023-01-25 07:39:37 +0300

    chngs cw fcc_js 25 Jan

--- FCC_Ivan_Nik.js
+++ FCC_Ivan_Nik.js
@@ -1343,6 +1343,38 @@ function isEveryoneHere(userObj) {
 
 console.log(isEveryoneHere(users));
 
+// Task_17 ==> Iterate Through the Keys of an Object with a for...in Statement
+const users = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+let sum = 0;
+for (let user in usersObj) {
+  if (usersObj[user].online == true) {
+    sum += 1;
+  }
+}
+return sum;
+  // Only change code above this line
+}
+
+console.log(countOnline(users));
+
+
+
+
+
+
 
 
 
